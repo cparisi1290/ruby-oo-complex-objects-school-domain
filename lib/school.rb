87c27a2214706148787db4c0a2c3b6664be599f6
Cassandra <cparisi1290@gmail.com>
2020-12-15 15:09:46 -0500

    commit tree 7fcc2536e9209aa501989b8c87b3379ccef395d8

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else 
+            @roster[grade] = [name]
+        end  
+    end
+
+    def grade(grade)
+        self.roster[grade]
+    end
+
+    def sort
+        self.roster.each do |grade, students|
+            students.sort!
+        end
+    end
+
+end
